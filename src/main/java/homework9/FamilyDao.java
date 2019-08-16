3599f21a822a2240f1420de80914042b02bf1fae
Maria101 <maria.bilous101@gmail.com>
2019-08-16 22:57:03 +0300

    Javacore Homework_9 structure created

---
+++ src/main/java/homework9/FamilyDao.java
@@ -0,0 +1,13 @@
+package homework9;
+
+import homework8.Family;
+
+import java.util.List;
+
+public interface FamilyDao {
+    List<Family> getAllFamilies();
+    Family getFamilyByIndex(int index);
+    boolean deleteFamily(int index);
+    boolean deleteFamily(Family f);
+    void saveFamily(Family f);
+}
---
+++ src/main/java/homework9/FamilyService.java
@@ -0,0 +1,70 @@
+package homework9;
+
+import homework8.Family;
+import homework8.Human;
+import homework8.Pet;
+
+import java.util.List;
+import java.util.Set;
+
+public class FamilyService {
+    private FamilyDao fDao;
+
+//    public List<Family> getAllFamilies() {
+//
+//    };
+
+    public void displayAllFamilies(){
+
+    }
+
+    public void getFamiliesBiggerThan(int num){
+
+    }
+
+    public void getFamiliesLessThan(int num){
+
+    }
+
+//    public int countFamiliesWithMemberNumber(int num){
+//
+//    }
+
+    public void createNewFamily(Human m, Human f){
+
+    }
+
+//    public boolean deleteFamilyByIndex(int index){
+//
+//    }
+
+//    public Family bornChild(Family f, String femName, String maleName){
+//
+//    }
+
+//    public Family adoptChild(Family f, Human h) {
+//
+//    }
+
+    public void deleteAllChildrenOlderThen(int age){
+
+    }
+
+//    public int count(){
+//
+//    }
+
+//    public Family getFamilyById(int index) {
+//
+//    }
+
+//    public Set<Pet> getPets(Family f){
+//
+//    }
+
+    public void addPet(int index, Pet pet){
+
+    }
+
+
+}
