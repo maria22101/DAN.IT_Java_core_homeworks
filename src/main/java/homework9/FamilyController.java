7e6e573b2a578c836b1699692c4fafb90479b2f5
Maria101 <maria.bilous101@gmail.com>
2019-08-18 12:47:19 +0300

    Javacore HW_9 FamilyController class completed

--- src/main/java/homework9/FamilyController.java
+++ src/main/java/homework9/FamilyController.java
@@ -1,5 +1,71 @@
 package homework9;
 
+import homework8.Family;
+import homework8.Human;
+import homework8.Pet;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class FamilyController {
-    FamilyService famService;
+    FamilyService famService = new FamilyService();
+
+    public List<Family> getAllFamilies() {
+        return famService.getAllFamilies();
+    }
+
+    public void displayAllFamilies() {
+        famService.displayAllFamilies();
+    }
+
+    public List<Family> getFamiliesBiggerThan(int num) {
+        return famService.getFamiliesBiggerThan(num);
+    }
+
+    public List<Family> getFamiliesLessThan(int num) {
+        return famService.getFamiliesLessThan(num);
+    }
+
+    public int countFamiliesWithMemberNumber(int num) {
+        return famService.countFamiliesWithMemberNumber(num);
+    }
+
+    public void createNewFamily(Human m, Human f) {
+        famService.createNewFamily(m, f);
+    }
+
+    public boolean deleteFamilyByIndex(int index) {
+        return famService.deleteFamilyByIndex(index);
+    }
+
+    public Family bornChild(Family f, String femName, String maleName) {
+        famService.bornChild(f, femName, maleName);
+        return f;
+    }
+
+    public Family adoptChild(Family f, Human h) {
+        famService.adoptChild(f, h);
+        return f;
+    }
+
+    public void deleteAllChildrenOlderThen(int age) {
+        famService.deleteAllChildrenOlderThen(age);
+    }
+
+    public int count() {
+        return famService.count();
+    }
+
+    public Family getFamilyById(int index) {
+        return famService.getFamilyById(index);
+    }
+
+    public Set<Pet> getPets(int index) {
+        return famService.getPets(index);
+    }
+
+    public void addPet(int index, Pet pet) {
+        famService.addPet(index, pet);
+    }
 }
