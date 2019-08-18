a6aaebf30b007cd082e845c5206b8499865b91d3
Maria101 <maria.bilous101@gmail.com>
2019-08-18 12:35:56 +0300

    Javacore HW_9 FamiliService class methods completed

--- src/main/java/homework9/FamilyService.java
+++ src/main/java/homework9/FamilyService.java
@@ -12,44 +12,44 @@ public class FamilyService {
 
     public List<Family> getAllFamilies() {
         return fDao.getAllFamilies();
-    };
+    }
 
-    public void displayAllFamilies(){
+    public void displayAllFamilies() {
         getAllFamilies().stream().forEach(System.out::println);
     }
 
-    public List<Family> getFamiliesBiggerThan(int num){
+    public List<Family> getFamiliesBiggerThan(int num) {
         List<Family> list = getAllFamilies().stream().filter(f -> f.countFamily() > num).collect(Collectors.toList());
         list.stream().forEach(System.out::println);
         return list;
     }
 
-    public List<Family> getFamiliesLessThan(int num){
+    public List<Family> getFamiliesLessThan(int num) {
         List<Family> list = getAllFamilies().stream().filter(f -> f.countFamily() < num).collect(Collectors.toList());
         list.stream().forEach(System.out::println);
         return list;
     }
 
-    public int countFamiliesWithMemberNumber(int num){
+    public int countFamiliesWithMemberNumber(int num) {
         List<Family> list = getAllFamilies().stream().filter(f -> f.countFamily() == num).collect(Collectors.toList());
         return list.size();
     }
 
-    public void createNewFamily(Human m, Human f){
+    public void createNewFamily(Human m, Human f) {
         fDao.createFamily(m, f);
     }
 
-    public boolean deleteFamilyByIndex(int index){
+    public boolean deleteFamilyByIndex(int index) {
         return fDao.deleteFamily(index);
     }
 
-    public Family bornChild(Family f, String femName, String maleName){
+    public Family bornChild(Family f, String femName, String maleName) {
         Human newChild;
         Random rand = new Random();
         if (rand.nextInt(2) == 0) {
             newChild = new Woman();
             newChild.setName(femName);
-        }else {
+        } else {
             newChild = new Man();
             newChild.setName(maleName);
         }
@@ -61,28 +61,44 @@ public class FamilyService {
     }
 
     public Family adoptChild(Family f, Human h) {
-        throw new IllegalArgumentException("not implemented yet");
+        Human newChild = h;
+        newChild.setFamily(f);
+        newChild.setSurname(f.getFather().getSurname());
+        f.getChildren().add(newChild);
+        fDao.saveFamily(f);
+        return f;
     }
 
-    public void deleteAllChildrenOlderThen(int age){
+    public void deleteAllChildrenOlderThen(int age) {
 
+        for (Family fam : getAllFamilies()) {
+            if (fam.getChildren().stream().filter(ch -> ch.getYear() > age).collect(Collectors.toList()).size() > 0) {
+                List<Human> newChildrenList = fam.getChildren()
+                        .stream()
+                        .filter(ch -> ch.getYear() <= age)
+                        .collect(Collectors.toList());
+                fam.setChildren(newChildrenList);
+                fDao.saveFamily(fam);
+            }
+        }
     }
 
-    public int count(){
-        throw new IllegalArgumentException("not implemented yet");
+    public int count() {
+        return getAllFamilies().size();
     }
 
     public Family getFamilyById(int index) {
-        throw new IllegalArgumentException("not implemented yet");
+        return fDao.getFamilyByIndex(index);
     }
 
-    public Set<Pet> getPets(Family f){
-        throw new IllegalArgumentException("not implemented yet");
+    public Set<Pet> getPets(int index) {
+        return fDao.getFamilyByIndex(index).getPet();
     }
 
-    public void addPet(int index, Pet pet){
-
+    public void addPet(int index, Pet pet) {
+        Family fam = fDao.getFamilyByIndex(index);
+        fam.getPet().add(pet);
+        fDao.saveFamily(fam);
     }
-
 
 }
